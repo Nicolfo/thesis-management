e619ae11547e007d7fa41a8496731d6be1dd342a
Nicolfo <nicolfo@hotmail.it>
2023-11-08 22:10:53 +0100

    backend now should work

--- src/main/java/it/polito/se2/g04/thesismanagement/application/Application.java
+++ src/main/java/it/polito/se2/g04/thesismanagement/application/Application.java
@@ -1,14 +1,18 @@
 package it.polito.se2.g04.thesismanagement.application;
 
 import it.polito.se2.g04.thesismanagement.attachment.Attachment;
+import it.polito.se2.g04.thesismanagement.proposal.Proposal;
 import it.polito.se2.g04.thesismanagement.student.Student;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Application {
     @Id
     @GeneratedValue
@@ -18,6 +22,13 @@ public class Application {
     @OneToOne
     private Attachment attachment;
     private Date applyDate;
+    @ManyToOne
+    private Proposal proposal;
 
-    //Add more if u wish
+    public Application(Student student, Attachment attachment, Date applyDate, Proposal proposal) {
+        this.attachment = attachment;
+        this.applyDate = applyDate;
+        this.proposal = proposal;
+    }
+//Add more if u wish
 }
--- src/main/java/it/polito/se2/g04/thesismanagement/application/ApplicationService.java
+++ src/main/java/it/polito/se2/g04/thesismanagement/application/ApplicationService.java
@@ -1,5 +1,6 @@
 package it.polito.se2.g04.thesismanagement.application;
 
 public interface ApplicationService {
+    public void applyForProposal(ApplicationDTO applicationDTO);
     //add method here
 }
