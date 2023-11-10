4cb0cef99c472611828cbdd0021bf9d534d0f152
Janosch <Janosch.ott@stud.tu-darmstadt.de>
2023-11-11 00:48:00 +0100

    Tests for backend of browse proposals

--- src/main/java/it/polito/se2/g04/thesismanagement/proposal/Proposal.java
+++ src/main/java/it/polito/se2/g04/thesismanagement/proposal/Proposal.java
@@ -2,21 +2,39 @@ package it.polito.se2.g04.thesismanagement.proposal;
 
 import it.polito.se2.g04.thesismanagement.group.Group;
 import it.polito.se2.g04.thesismanagement.teacher.Teacher;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Proposal {
+    public Proposal(String title, Teacher supervisor, List<Teacher> coSupervisors, String keywords, String type, List<Group> groups, String description, String requiredKnowledge, String notes, Date expiration, String level, String cdS, Boolean archived) {
+        this.title = title;
+        this.supervisor = supervisor;
+        this.coSupervisors = coSupervisors;
+        this.keywords = keywords;
+        this.type = type;
+        this.groups = groups;
+        this.description = description;
+        this.requiredKnowledge = requiredKnowledge;
+        this.notes = notes;
+        this.expiration = expiration;
+        this.level = level;
+        CdS = cdS;
+        this.archived = archived;
+    }
+
     @Id
+    @GeneratedValue
     private Long id;
     private String title;
     @ManyToOne
