d76d568474489070ea8ba02dab38b7e38267a8ec
Nicolfo <nicolfo@hotmail.it>
2023-11-11 17:58:40 +0100

    fixed cors

--- src/main/java/it/polito/se2/g04/thesismanagement/security/SecurityConfig.java
+++ src/main/java/it/polito/se2/g04/thesismanagement/security/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -46,6 +47,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(AbstractHttpConfigurer::disable)
+                .cors(it->it.configurationSource(request -> {
+                    CorsConfiguration corsConfig = new CorsConfiguration();
+                    corsConfig.addAllowedOrigin("http://localhost:3000");
+                    corsConfig.addAllowedOrigin("http://127.0.0.1:3000");
+                    corsConfig.addAllowedMethod("*");
+                    corsConfig.addAllowedHeader("*"); // Allow all headers
+                    return corsConfig;
+                }))
                 .authorizeHttpRequests(auth->{
                     auth.requestMatchers("/API/login","/API/register").permitAll()
                             .anyRequest().authenticated();
