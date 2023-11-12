e458b8b44c9aa8f9c4905ae4d1061f6523ef7f3e
Nicolfo <nicolfo@hotmail.it>
2023-11-12 20:13:57 +0100

    fixed attachment update/retrival

--- src/main/java/it/polito/se2/g04/thesismanagement/attachment/AttachmentServiceImpl.java
+++ src/main/java/it/polito/se2/g04/thesismanagement/attachment/AttachmentServiceImpl.java
@@ -1,26 +1,28 @@
 package it.polito.se2.g04.thesismanagement.attachment;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class AttachmentServiceImpl implements AttachmentService{
     private final AttachmentRepository attachmentRepository;
 
-    public AttachmentServiceImpl(AttachmentRepository attachmentRepository) {
-        this.attachmentRepository = attachmentRepository;
-    }
 
     @Override
     public AttachmentDTO addAttachment(MultipartFile file) throws IOException {
-        var toAdd=new Attachment(file.getBytes(),file.getContentType(),file.getOriginalFilename());
-        return attachmentRepository.save(toAdd).toDto();
+        var toAdd = new Attachment(file.getBytes(), file.getContentType(), file.getOriginalFilename());
+        Attachment savedAttachment = attachmentRepository.save(toAdd);
+        return savedAttachment.toDto();
     }
 
     @Override
     public Attachment getFileById(Long id) {
-        return attachmentRepository.getReferenceById(id);
+        Optional<Attachment> optionalAttachment = attachmentRepository.findById(id);
+        return optionalAttachment.orElse(null);
     }
 }
