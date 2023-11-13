d41369469f26f0ae89b597197579221e577be35e
Janosch <Janosch.ott@stud.tu-darmstadt.de>
2023-11-13 23:35:26 +0100

    add JUNIT Tests

--- src/main/java/it/polito/se2/g04/thesismanagement/proposal/Proposal.java
+++ src/main/java/it/polito/se2/g04/thesismanagement/proposal/Proposal.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,6 +16,7 @@ import java.util.List;
 @Entity
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Proposal {
     @Id
     private Long id;
