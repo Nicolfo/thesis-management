9863fd94ce2b6bdd42de74859bdc7ab3aa5ed905
Janosch <Janosch.ott@stud.tu-darmstadt.de>
2023-11-18 01:04:27 +0100

    Update Navbar to make it more responsive

--- frontend/src/SideBar/SideBar.js
+++ frontend/src/SideBar/SideBar.js
@@ -1,5 +1,7 @@
-import {useNavigate,useLocation} from 'react-router-dom';
-function SideBar(props){
+import {useNavigate, useLocation} from 'react-router-dom';
+import {Button, Nav, Navbar, Container, Col} from 'react-bootstrap';
+
+function SideBar(props) {
     const path = useLocation().pathname;
     const navigate = useNavigate();
 
@@ -9,50 +11,67 @@ function SideBar(props){
     const userIsStudent = () => {
         return props.user && props.user.role === "STUDENT";
     }
-    if(props.user)
-    return (
-        <div className="d-flex flex-column bg-light p-2 col-2">
-
-                <ul className="nav nav-pills flex-column mb-auto nav-fill ">
-                    {userIsStudent()&&<>
-                    <li className="nav-item">
-                        <button className={path==='/search-for-proposal' ? "nav-link active link-light text-start":"nav-link link-dark text-start"} onClick={()=>{navigate('/search-for-proposal'); props.searchForProposalClicked()}} >
-                            Search for proposal
-                        </button>
-                    </li>
-                    <li className="nav-item">
-                        <button className={path==='/browseDecisions' ? "nav-link active link-light text-start":"nav-link link-dark text-start"} onClick={()=>{navigate('/browseDecisions')}} >
-                            My applications decisions
-                        </button>
-                    </li>
-                    </>
-                    }
-                    {
-                        userIsTeacher() &&<>
-                        <li className="nav-item">
-                        <button className={path==='/teacher/proposals' ? "nav-link active link-light text-start":"nav-link link-dark text-start"} onClick={()=>{navigate('/teacher/proposals')}} >
-                            My thesis proposals
-                        </button>
-                        </li>
-
-                    <li className="nav-item">
-                        <button className={path==='/insertProposal' ? "nav-link active link-light text-start":"nav-link link-dark text-start"} onClick={()=>{navigate('/insertProposal')}} >
-                            Insert Proposal
-                        </button>
-                    </li>
-                    <li className="nav-item">
-                        <button className={path==='/teacher/application/browse'|| path==='/teacher/application/view' ? "nav-link active link-light text-start":"nav-link link-dark text-start"} onClick={()=>{navigate('/teacher/application/browse')}} >
-                            My application proposals
-                        </button>
-                    </li>
-                        </>
-                    }
-
-                </ul>
-        </div>
-
-    )
-
+    if (props.user)
+        return (
+            <Navbar bg="light" expand={false}>
+                <Container fluid>
+                    <Navbar.Toggle aria-controls="navbar-nav"/>
+                    <Navbar.Collapse id="navbar-nav">
+                        <Nav className="flex-column">
+                            {userIsStudent() && <>
+                                <Col xs={12}>
+                                    <Button variant="light" block
+                                            className={path === '/search-for-proposal' ? "active" : ""} onClick={() => {
+                                        navigate('/search-for-proposal');
+                                        props.searchForProposalClicked()
+                                    }}>
+                                        Search for proposal
+                                    </Button>
+                                </Col>
+                                <Col xs={12}>
+                                    <Button variant="light" block
+                                            className={path === '/browseDecisions' ? "active" : ""} onClick={() => {
+                                        navigate('/browseDecisions')
+                                    }}>
+                                        My applications decisions
+                                    </Button>
+                                </Col>
+                            </>
+                            }
+                            {
+                                userIsTeacher() && <>
+                                    <Col xs={12}>
+                                        <Button variant="light" block
+                                                className={path === '/teacher/proposals' ? "active" : ""} onClick={() => {
+                                            navigate('/teacher/proposals')
+                                        }}>
+                                            My thesis proposals
+                                        </Button>
+                                    </Col>
+                                    <Col xs={12}>
+                                        <Button variant="light" block className={path === '/insertProposal' ? "active" : ""}
+                                                onClick={() => {
+                                                    navigate('/insertProposal')
+                                                }}>
+                                            Insert Proposal
+                                        </Button>
+                                    </Col>
+                                    <Col xs={12}>
+                                        <Button variant="light" block
+                                                className={path === '/teacher/application/browse' || path === '/teacher/application/view' ? "active" : ""}
+                                                onClick={() => {
+                                                    navigate('/teacher/application/browse')
+                                                }}>
+                                            My application proposals
+                                        </Button>
+                                    </Col>
+                                </>
+                            }
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        )
 }
 
 
