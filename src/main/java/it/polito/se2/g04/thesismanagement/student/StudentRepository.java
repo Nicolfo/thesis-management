608a9268e3e8dec53237b5f2913a6fb2597057c5
Nicolfo <nicolfo@hotmail.it>
2023-12-13 20:24:04 +0100

    Update StudentRepository.java

--- src/main/java/it/polito/se2/g04/thesismanagement/student/StudentRepository.java
+++ src/main/java/it/polito/se2/g04/thesismanagement/student/StudentRepository.java
@@ -3,6 +3,6 @@ package it.polito.se2.g04.thesismanagement.student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepository extends JpaRepository<Student,Long> {
-    public Boolean existsByEmail(String email);
+    public boolean existsByEmail(String email);
     public Student getStudentByEmail(String email);
 }
