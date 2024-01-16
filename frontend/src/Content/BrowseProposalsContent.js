dfa297dcbb3bd73063cb2d092d98faae8f92b7ed
veronicamattei <veronica.mattei@studenti.polito.it>
2024-01-16 17:31:47 +0100

    Change pointer

--- frontend/src/Content/BrowseProposalsContent.js
+++ frontend/src/Content/BrowseProposalsContent.js
@@ -206,7 +206,7 @@ function ProposalAccordion({proposal, setDeleting, setDeletingID, user, setArchi
                                     </>
                                 ) : (
                                     <>
-                                        <Card.Text onClick={() => setTruncated(true)}>{proposal.description} <span style={{ color: '#FC7A08', cursor: 'pointer' }}>See less</span></Card.Text>
+                                        <Card.Text onClick={() => setTruncated(true)} className="cursor-pointer">{proposal.description} <span style={{ color: '#FC7A08' }}>See less</span></Card.Text>
                                     </>
                                 )}
                             </Col>
